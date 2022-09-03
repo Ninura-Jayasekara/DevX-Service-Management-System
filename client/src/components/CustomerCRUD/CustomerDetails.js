0531d67e99c09219c3f2fa58f63ef4469f0e0ee3
InsafNilam <insafnilam.2000@gmail.com>
2022-09-03 17:53:27 +0530

    Added-Customer-CRUD-Frontend

---
+++ client/src/components/CustomerCRUD/CustomerDetails.js
@@ -0,0 +1,178 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+} from "@mui/material";
+
+import SearchIcon from "@mui/icons-material/Search";
+
+function CustomerTable() {
+  const createData = (name, calories, fat, carbs, protein) => {
+    return { name, calories, fat, carbs, protein };
+  };
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
+
+  return (
+    <Container>
+      <Wrap>
+        <InputComponent>
+          <div className="table-head">Customer Details</div>
+          <InputGroup>
+            <SearchIcon />
+            <input type="text" placeholder="Search" />
+          </InputGroup>
+        </InputComponent>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead sx={{ background: "#36454f" }}>
+              <TableRow>
+                <TableCell>NIC</TableCell>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Email</TableCell>
+                <TableCell align="right">Phone&nbsp;Number</TableCell>
+                <TableCell align="right">Address</TableCell>
+                <TableCell align="right">Date</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                  <TableCell align="right">2012</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <ButtonGroup>
+          <Link to="add">
+            <button>Add</button>
+          </Link>
+          <Link to="edit">
+            <button>Edit</button>
+          </Link>
+          <Link to="delete">
+            <button>Delete</button>
+          </Link>
+          <Link to="">
+            <button>Report</button>
+          </Link>
+        </ButtonGroup>
+      </Wrap>
+    </Container>
+  );
+}
+
+export default CustomerTable;
+
+const Container = styled.main`
+  min-height: calc(100vh);
+  padding: 60px calc(3vw) 0px;
+  overflow-x: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrap = styled.div`
+  padding: 10px calc(0.5vw + 5px);
+  background: #151e3d;
+  border-radius: 12px;
+  width: 100%;
+  min-height: 50vh;
+`;
+
+const InputComponent = styled.div`
+  display: flex;
+  padding: 10px 0;
+  div.table-head {
+    flex: 1;
+    text-transform: uppercase;
+    font-size: 20px;
+    position: relative;
+
+    &:after {
+      position: absolute;
+      content: "";
+      bottom: 0px;
+      left: 0;
+      width: 40px;
+      height: 2px;
+      background: #733635;
+    }
+  }
+
+  @media (max-width: 570px) {
+    flex-direction: column;
+    div.table-head {
+      margin: 0 auto 10px;
+
+      &:after {
+        display: none;
+      }
+    }
+  }
+`;
+const InputGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  input {
+    border-radius: 5px;
+    color: #f5f5f5;
+    font-size: 16px;
+    padding: 6px;
+    outline: none;
+    border: none;
+    background: #404040;
+    transition: all 0.3s ease 0s;
+
+    &:focus {
+      box-shadow: 0 0 0 2px #909090;
+    }
+  }
+`;
+
+const ButtonGroup = styled.div`
+  padding: 6px 0 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  button {
+    cursor: pointer;
+    padding: 5px;
+    margin: 0 3px;
+    background: #404040;
+    border: none;
+    border-radius: 5px;
+    transition: all 0.3s ease 0s;
+
+    &:hover {
+      box-shadow: 0 0 0 2px #909090;
+    }
+  }
+`;
