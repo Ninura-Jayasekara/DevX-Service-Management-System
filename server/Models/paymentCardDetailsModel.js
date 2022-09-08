756687b06f008553f692488f617fba8db8d378e7
Manuka-Dahanayake <manukaud98@gmail.com>
2022-09-08 11:17:42 +0530

    commit card detail model

---
+++ server/Models/paymentCardDetailsModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const cardDetailSchema = new Schema({
+
+    userName: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+
+    cardNumber: {
+        type: String,
+        required: [true, 'Card Number is required']
+    },
+
+    expDate: {
+        type: Date,
+        required: [true, 'Expiry Date is required']
+    },
+
+    cvc: {
+        type: String,
+        required: [true, 'CVC number is required']
+    },
+
+    cardType: {
+        type: String,
+        required: [true]
+    },
+
+    amount: {
+        type: Number,
+        required: [true, 'Amount is required']
+    }
+
+})
+
+const CardDetails = mongoose.model("CardDetails" ,cardDetailSchema)
+
+module.exports = CardDetails;
