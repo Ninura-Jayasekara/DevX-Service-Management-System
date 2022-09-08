40c58959630af074f72c0d07703528c186a47179
Mishan Perera <mishkavinduperera65@gmail.com>
2022-09-08 11:01:48 +0530

    I've added deletefacility method beacuse of implement the delete facility function

--- server/Routes/serviceFacilityRoutes.js
+++ server/Routes/serviceFacilityRoutes.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const {
     addFacility,
     viewFacilities,
-    updateFacility
+    updateFacility,
+    deleteFacility
 } = require('../Controllers/serviceFaciltyController')
 
 router.get('/', viewFacilities)
 router.post('/add', addFacility)
 router.put('/update/:fId', updateFacility)
+router.delete('/delete/:fId', deleteFacility)
 
 module.exports = router
